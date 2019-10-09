fd786de1a6bf588dc1b219a62e25a54b7bfd44cd
Jesus Solis <solisjesus700@gmail.com>
2019-10-08 22:05:09 -0400

    shuffles players before starting simulation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sim
+import random
 
 elos = [955,965,975,985,995,1005,1015,1025,1035,1045]
 players = [sim.Player(elos[i]) for i in range(10)]
@@ -9,6 +10,8 @@ players = [sim.Player(elos[i]) for i in range(10)]
 # sim.sort_players(players)
 
 # print(players)
+
+random.shuffle(players)
 
 for i in range(10):
 	print("doing round " + str(i))
