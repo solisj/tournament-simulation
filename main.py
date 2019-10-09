2a2ddc2a4575d1d15f4ae966081054190e9d336a
Jesus Solis <solisjesus700@gmail.com>
2019-10-08 21:16:19 -0400

    finishing touches

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ import sim
 elos = [955,965,975,985,995,1005,1015,1025,1035,1045]
 players = [sim.Player(elos[i]) for i in range(10)]
 
-print(players)
+# print(players)
 
-sim.do_round(players)
-sim.sort_players(players)
+# sim.do_round(players)
+# sim.sort_players(players)
 
-print(players)
+# print(players)
+
+for i in range(10):
+	print("doing round " + str(i))
+	print(players)
+	sim.do_round(players)
+	sim.sort_players(players)
+	print(players)
+	print("\n")
